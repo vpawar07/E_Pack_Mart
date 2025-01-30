3c1ed8b95a3d39827fe2b27a0fd47951851f77d5
vpawar07 <vp7600634@gmail.com>
2025-01-30 17:30:14 +0530

    Authentication API

---
+++ p15_CRUD/src/main/java/com/example/demo/entity/Category.java
@@ -0,0 +1,48 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="category")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int cat_id;
+	
+	String cat_name;
+
+	public Category() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Category(int cat_id, String cat_name) {
+		super();
+		this.cat_id = cat_id;
+		this.cat_name = cat_name;
+	}
+
+	public int getCat_id() {
+		return cat_id;
+	}
+
+	public void setCat_id(int cat_id) {
+		this.cat_id = cat_id;
+	}
+
+	public String getCat_name() {
+		return cat_name;
+	}
+
+	public void setCat_name(String cat_name) {
+		this.cat_name = cat_name;
+	}
+	
+	
+	
+}
---
+++ p15_CRUD/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,58 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.City;
+import com.example.demo.entity.LoginHelper;
+import com.example.demo.entity.Role;
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserRepository userrepo;
+	
+	public List<User> getAllUser(){
+		return userrepo.findAll();
+	}
+	
+	public User insertCustomer(User cust) {
+		return userrepo.save(cust);
+	}
+	
+	public User loginCheck(LoginHelper obj) {
+		System.out.println(obj);
+		User u;
+		Optional<User> ol = userrepo.loginCheck(obj.getEmail_id(), obj.getPassword());
+		try {
+			u = ol.get();
+		}
+		catch(Exception e){
+			u = null;
+		}
+		return u;
+	}
+	
+	public int updateCustomer(int user_id, String email, String name, String password, City city_id, String address, String pancard, Role role_id) {
+		System.out.println("called");
+		return userrepo.updateUser(user_id, email, name, password, city_id, address, pancard, role_id);
+	}
+
+	public int updateComp(int comp_id, String email, String name, String password, City city_id, String address, String pancard, Role role_id, String msme_cert_no, String gst_no) {
+		int user_id= userrepo.getUserIdByComp(comp_id);
+		userrepo.updateUser(user_id, email, name, password, city_id, address, pancard, role_id);
+		User u = userrepo.findById(user_id).orElse(null);
+		return userrepo.updateComp(comp_id, msme_cert_no, gst_no, u);
+	}
+}
+
+
+
+
+
